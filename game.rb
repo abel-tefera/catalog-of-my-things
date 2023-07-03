ecb6e49913374fc9245207b37504d1a39705b6bb
jeddaa <fathiato@gmail.com>
2023-07-03 10:06:51 +0100

    creating game/author class

---
+++ game.rb
@@ -0,0 +1,11 @@
+class Game
+  attr_accessor:
+  def initialize (multiplayer, last_played_at)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    @last_played_at < 1.year.ago
+  end
+end
