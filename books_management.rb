da8f38dc6457b7245cb1ea5f650c308b3f83ce87
abel-tefera <abeltefera16@gmail.com>
2023-07-04 17:22:36 +0300

    Created add & list book functionality

--- books_management.rb
+++ books_management.rb
@@ -49,4 +49,47 @@ def save_label
   File.write('db/labels.json', JSON.pretty_generate(labels_arr))
 end
 
+def add_book
+  puts 'What is the title of the book?'
+  title = gets.chomp
 
+  puts 'What is the color of the book?'
+  color = gets.chomp
+
+  puts 'Who is the publisher?'
+  publisher = gets.chomp
+
+  puts 'What is the condition of the cover? (good/bad)'
+  cover_state = gets.chomp.downcase
+
+  puts 'On what date was the book published?'
+  published_at = gets.chomp
+
+  book = Book.new(publisher, cover_state, published_at)
+  @books << book
+
+  label = Label.new(title, color)
+  label.add_item(book)
+
+  @labels << label
+
+  save_book
+
+  save_label
+
+  puts 'Book created successfully!'
+end
+
+def list_books
+  @books.each do |book|
+    puts '------------------------------'
+    puts "Publisher: #{book.publisher} "
+    puts "Cover Condition: #{book.cover_state} "
+    puts "Published on: #{book.publish_date}"
+    puts '------------------------------'
+  end
+end
+
+def list_labels
+  @labels.each { |label| puts "Title: #{label.title} color: #{label.color}" }
+end
