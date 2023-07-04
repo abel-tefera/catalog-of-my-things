85c69248cfff268d63ab4807d3ca7cf04157eb22
abel-tefera <abeltefera16@gmail.com>
2023-07-04 10:59:05 +0300

    Linters fix

--- home.rb
+++ home.rb
@@ -1,38 +1,37 @@
 def menu(app)
-    app.options
-    print 'Prompt > '
-    gets.chomp.to_i
+  app.options
+  print 'Prompt > '
+  gets.chomp.to_i
 end
-  
-def manage_selection(app, option)
-    tasks = {
-        1 => :list_books,
-        2 => :list_albums,
-        3 => :list_games,
-        4 => :list_genres,
-        5 => :list_labels,
-        6 => :list_authors,
-        7 => :list_sources,
-        8 => :add_book,
-        9 => :add_album,
-        10 => :add_game,
-        11 => :save_and_exit,
-        default: :invalid_option
-    }
 
-    selection = tasks[option] || tasks[:default]
-    app.send(selection)
+def manage_selection(app, option)
+  tasks = {
+    1 => :list_books,
+    2 => :list_albums,
+    3 => :list_games,
+    4 => :list_genres,
+    5 => :list_labels,
+    6 => :list_authors,
+    7 => :list_sources,
+    8 => :add_book,
+    9 => :add_album,
+    10 => :add_game,
+    11 => :save_and_exit,
+    default: :invalid_option
+  }
+
+  selection = tasks[option] || tasks[:default]
+  app.send(selection)
 end
 
 def home(app)
-      
-    puts '------------------------------'
-    puts 'Welcome to Catalog of my things!'
-    puts '------------------------------'
+  puts '------------------------------'
+  puts 'Welcome to Catalog of my things!'
+  puts '------------------------------'
 
-    loop do
-        option = menu(app)
-        manage_selection(app, option)
-        puts "\n"
-    end
+  loop do
+    option = menu(app)
+    manage_selection(app, option)
+    puts "\n"
+  end
 end
--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require_relative 'home'
 def main
   app = App.new
   app.fetch_json_files
-  
+
   home(app)
 end
 
