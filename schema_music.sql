0185462a84eaba443ac3ec404f40daf29bff9146
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-04 22:22:49 +0000

    Add schema for music_album and genre tables

---
+++ schema_music.sql
@@ -0,0 +1,14 @@
+-- Create music_albums table
+CREATE TABLE music_albums (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  genre_id INT REFERENCES genre(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  publish_data DATE NOT NULL,
+  archived BIT NOT NULL,
+  on_spotify BIT NOT NULL
+);
+
+-- Create genres table
+CREATE TABLE genres (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  genre_name VARCHAR(50) NOT NULL,
+);
